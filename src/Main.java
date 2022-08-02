ec0adfb0667b53bd9f220704b1d0d417fc8b9dac
RicardoJun10r <ricardojunior0110@gmail.com>
2022-08-02 20:08:50 -0300

    Enviando pane principal e a Main configurada

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -20,5 +25,14 @@ public class Main extends Application{
     public void start(Stage pS) throws Exception{
         setPs(pS);
         pS.show();
+        telaInicio();
+    }
+
+    public static void telaInicio() throws IOException
+    {
+        Parent root = FXMLLoader.load(Main.class.getResource("/view/Inicio.fxml"));
+        Scene cena = new Scene(root);
+        pS.setTitle("Inicio");
+        pS.setScene(cena);
     }
 }
